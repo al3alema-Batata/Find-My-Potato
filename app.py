7bbcf9a28bdcdecb6fa4c19dffaa70b552ea75ce
al3alema-Batata <137813106+al3alema-Batata@users.noreply.github.com>
2023-07-07 18:41:48 +0300

    changed apiKey writing, and tpasted chatGPT answer to chatting

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ import cohere
 import apikey
 
 # initialize the Cohere Client with an API Key
-co = cohere.Client(apikey.apiKey)
+cohere.api_key=apikey.apiKey
 
 
-# generate a prediction for a prompt
-# prediction = co.generate(
-#             model='large',
-#             prompt='co:here',
-#             max_tokens=10)
 
-# print the predicted text
-# print('prediction: {}'.format(prediction.generations[0].text))
+#from chatGPT trying to open a chat model
+#Define a function that interacts with the Cohere API to generate a response
+def generate_response(input_text):
+    prompt = "Your bot prompt: " + input_text
+    model = 'gpt-3.5-turbo'
+    response = cohere.Completion.create(prompt=prompt, model=model)
+    return response['choices'][0]['text']
+
+#Create a function that handles incoming messages and generates a response using the Cohere API:
+def handle_message(message):
+    response = generate_response(message)
+    print("Bot:", response)
+
+#In the main function, prompt the user for input and call the handle_message function:
+def main():
+    while True:
+        user_input = input("You: ")
+        handle_message(user_input)
+
+if __name__ == "__main__":
+            main()
