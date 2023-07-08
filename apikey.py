ce7a246d00a0892f984b4ae440303f1cebfc60d7
al3alema-Batata <137813106+al3alema-Batata@users.noreply.github.com>
2023-07-08 16:50:10 +0300

    commented most of the stuuf

--- apikey.py
+++ apikey.py
@@ -1,1 +1,3 @@
 apikey="6UKve6jEMrkvR7TB0hUxL9t1UZQxJchCMzQuXJB5"
+#place
+apikeyG="AIzaSyAozwtyxEvGXazxGVXXZj9-jVbUT4p9Emc"
--- main.py
+++ main.py
@@ -1,22 +1,60 @@
 import cohere
 import streamlit as st
-from apikey import apikey
+import requests
+from apikey import apikey, apikeyG
+from geopy.geocoders import GoogleV3
+
+# Initialize Google Maps geocoder
+geocoder = GoogleV3(api_key=apikeyG)
+
+
 # Writing a prompt for my model
 def myPrompt(question):
     return 'You are a Friendly Tour guide in KSA that answers questions:'
 
-def batataBot(question):
-    cohere_instance = cohere.Client(apikey, '2021-11-08')
+
+def search_places(query):
+    url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+    params = {
+        'key': apikeyG,
+        'query': query
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['results']
+
+
+def get_reviews(place_id):
+    url = 'https://maps.googleapis.com/maps/api/place/details/json'
+    params = {
+        'key': apikeyG,
+        'place_id': place_id,
+        'fields': 'reviews'
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['result']['reviews']
+
+
+def get_place_map_url(place_id):
+    return f"https://www.google.com/maps/place/?q=place_id:{place_id}"
+
+
+def batataBot(question, search_query):
+    cohere_instance = cohere.Client(api_key=apikey, version='2021-11-08')
+    prompt = myPrompt(question) + f"\n\nSearch for '{search_query}' in Google Maps and provide reviews."
     response = cohere_instance.generate(
         model='medium',
-        prompt=myPrompt(question),
-        max_tokens=50,
-        temperature=1
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.8
     ).generations[0].text
     return response
 
+
 # Streamlit app configuration
 st.set_page_config(page_title="BatataBot", layout="wide")
+
 
 # Writing the app main function
 def main():
@@ -25,28 +63,53 @@ def main():
     st.write("Ask any question, and BatataBot will provide answers!")
 
     # Available place types for the checklist
-    place_types = ['Historical Sites', 'Beaches', 'Mountains', 'Deserts', 'Cities']
+    place_types = ['Shopping', 'Food', 'Activities']
 
-    # User selection for the type of place to visit
-    selected_types = st.multiselect("Select the type of place you want to visit", place_types)
+    # User selection for the type of places to visit
+    selected_types = st.multiselect("Select the type of places you want to visit", place_types)
 
-    # User input for ideal place or preferences
-    user_preferences = st.text_input("Enter your ideal place or preferences")
+    # User input for preferences
+    user_preferences = st.text_input("Enter your preferences")
 
-    if st.button("Ask"):
-        # Generating response from BatataBot
-        response = batataBot(user_preferences)
+    # if st.button("Search"):
+    #     # Combine selected types and user preferences into a search query
+    #     search_query = 'Shopping'
+    #
+    #     # Searching for places in Google Maps
+    #     places = search_places(search_query)
+    #
+    #     if places:
+    #         st.subheader("Best Place:")
+    #         selected_place = places[0]
+    #         st.write(f"Name: {selected_place['name']}")
+    #         st.write(f"Address: {selected_place['formatted_address']}")
+    #         st.write(f"Rating: {selected_place['rating']}")
+    #
+    #         # Displaying the place on a map
+    #         place_coordinates = selected_place['geometry']['location']
+    #         st.write(f"Location: ({place_coordinates['lat']}, {place_coordinates['lng']})")
+    #         map_url = get_place_map_url(selected_place['place_id'])
+    #         st.markdown(f"[Show on Map]({map_url})")
+    #
+    #         # Getting reviews for the selected place
+    #         reviews = get_reviews(selected_place['place_id'])
+    #
+    #         if reviews:
+    #             st.subheader("Reviews:")
+    #             for review in reviews:
+    #                 st.write(f"- {review['text']}")
+    #         else:
+    #             st.write("No reviews found for this place.")
+    #     else:
+    #         st.write("No places found matching your preferences.")
+    #
+    # if st.button("Ask"):
+    #     # Generating response from BatataBot
+    #     response = batataBot(user_preferences, user_preferences)
+    #
+    #     # Displaying the response
+    #     st.write("BatataBot:", response)
 
-        # Displaying the response
-        st.write("BatataBot:", response)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
